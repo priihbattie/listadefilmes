e40d05991fabd4cd6bb2141105eb7a6d3a8a9a56
priihbattie <135707297+priihbattie@users.noreply.github.com>
2024-10-04 12:01:14 -0300

    Update conta.java

--- conta.java
+++ conta.java
@@ -7,4 +7,12 @@ class Conta {
     public void deposita(double valor){
         this.saldo = this.saldo + valor;
     }
+        public boolean saca(double valor) { 
+        if(this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else { 
+            return false;
+        }
+    }
 }
